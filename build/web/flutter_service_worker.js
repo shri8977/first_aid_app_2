94cf87a1998df2587f6de1b80c37e88ff8930ae7
shri subashini <shrisubashini2007@gmail.com>
2025-09-07 10:42:49 +0530

    Add Flutter web build for deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7df9c7881dbf1750dc76ffa5858ba09c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8747da28a593f2789d801ac8620bfcf0",
+"flutter_bootstrap.js": "6737dcbd0676f89c05a04dc610d42a79",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
